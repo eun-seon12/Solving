307ec27d5b718b45470441f223c05e4d3085957d
I7E-72\i7E-63 <ces234021@gmail.com>
2025-07-22 17:41:04 +0900

    0722

---
+++ solving/src/main/java/com/teamname/problem/bj/test1008.java
@@ -0,0 +1,16 @@
+package com.teamname.problem.bj;
+
+import java.util.Scanner;
+
+public class test1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num1 =sc.nextDouble();
+
+        double num2 = sc.nextDouble();
+
+
+        System.out.println(num1/num2);
+    }
+}
