5b9c5cc9e4d34d625f48ae6dd6dca1c6b6df9d8b
DESKTOP-3OSO63K\os <ces234021@gmail.com>
2025-07-24 22:28:15 +0900

    0724 test

---
+++ solving/src/main/java/com/teamname/problem/bj/test10869.java
@@ -0,0 +1,20 @@
+package com.teamname.problem.bj.solving.src.main.java.com.teamname.problem.bj;
+
+import java.util.Scanner;
+
+public class test10869 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+    }
+}
