106fd25a0c15fa50c7163ad253281a94dc471f2a
DESKTOP-3OSO63K\os <ces234021@gmail.com>
2025-07-26 03:25:12 +0900

    0726 test

---
+++ solving/src/main/java/com/teamname/problem/pm/printString.java
@@ -0,0 +1,14 @@
+package com.teamname.problem.bj.solving.src.main.java.com.teamname.problem.pm;
+
+import java.util.Scanner;
+
+public class printString {
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+
+            System.out.println(a);
+        }
+
+}
