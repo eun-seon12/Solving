23ba42272d86eee35aeb3785d7d6371e925efaa8
eun-seon12 <ces234021@gmail.com>
2025-09-06 23:56:01 +0900

    Add files via upload

---
+++ test10871.java
@@ -0,0 +1,22 @@
+package com.teamname.problem.bj.solving.src.main.java.com.teamname.problem.bj;
+
+import java.util.Scanner;
+
+public class test10871{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        for(int i = 0; i < N; i++){
+            if(arr[i] < X){
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
